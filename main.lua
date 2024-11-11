fec082902fac63c0534f83f002d21bbe9a3b585b
Anson-Mah-ACITECH <ansonmah212@gmail.com>
2024-11-10 19:31:35 -0500

    10 November 2024 7:31PM

---
+++ main.lua
@@ -0,0 +1,57 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Among Us for All Suits
+--- MOD_ID: AmongUsAllSuits
+--- MOD_AUTHOR: [Jersen]
+--- MOD_DESCRIPTION: Adds the Among Us collab to the other three suits
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+G.COLLABS.options.Clubs[#G.COLLABS.options.Clubs + 1] = "collab_AU_clubs"
+G.COLLABS.options.Diamonds[#G.COLLABS.options.Diamonds + 1] = "collab_AU_diamonds"
+G.COLLABS.options.Spades[#G.COLLABS.options.Spades + 1] = "collab_AU_spades"
+
+SMODS.Atlas {
+	key = "collab_AU_clubs_1",
+	path = "collab_AU_clubs_1.png",
+	px = 71, 
+	py = 95,
+}
+
+SMODS.Atlas {
+	key = "collab_AU_clubs_2",
+	path = "collab_AU_clubs_2.png",
+	px = 71, 
+	py = 95,
+}
+
+SMODS.Atlas {
+	key = "collab_AU_diamonds_1",
+	path = "collab_AU_diamonds_1.png",
+	px = 71, 
+	py = 95,
+}
+
+SMODS.Atlas {
+	key = "collab_AU_diamonds_2",
+	path = "collab_AU_diamonds_3.png",
+	px = 71, 
+	py = 95,
+}
+
+SMODS.Atlas {
+	key = "collab_AU_spades_1",
+	path = "collab_AU_spades_1.png",
+	px = 71, 
+	py = 95,
+}
+
+SMODS.Atlas {
+	key = "collab_AU_spades_2",
+	path = "collab_AU_spades_2.png",
+	px = 71, 
+	py = 95,
+}
+
+----------------------------------------------
+------------MOD CODE END----------------------
